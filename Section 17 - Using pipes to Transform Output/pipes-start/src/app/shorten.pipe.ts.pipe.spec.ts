a4f292ceca2f994b84c2f6c717dc8a53f546e2a4
maryn1421 <56552980+maryn1421@users.noreply.github.com>
2021-07-22 12:41:37 +0200

    finished the pipes section

---
+++ Section 17 - Using pipes to Transform Output/pipes-start/src/app/shorten.pipe.ts.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { Shorten.Pipe.TsPipe } from './shorten.pipe.ts.pipe';
+
+describe('Shorten.Pipe.TsPipe', () => {
+  it('create an instance', () => {
+    const pipe = new Shorten.Pipe.TsPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
